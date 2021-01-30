30a9e95b15f0fc7563314b78e275483e6ff388e8
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-01-30 10:46:13 -0500

    Add OpenAPI title and version

--- mouse_trapi/server.py
+++ mouse_trapi/server.py
@@ -3,7 +3,10 @@ from fastapi import Body, FastAPI
 
 from .parse import parse_question
 
-app = FastAPI()
+app = FastAPI(
+    title="Mouse-TRAPI",
+    version="1.0.0",
+)
 
 
 @app.post("/to_trapi")
